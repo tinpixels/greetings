2a057a1f6f8c07e9b429a4a35fa03e158576f6c0
tinpixels <tinpixels@gmail.com>
2017-01-07 18:46:35 -0900

    Works!

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+// Created by Garrett Zimpelman on 1/7/17.
+//
+
+#include <flac/1.3.2/include/FLAC/stream_decoder.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("Greetings.\n");
+    printf("All your base are belong to us!\n");
+
+    getchar();
+}
